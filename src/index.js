914e74c519f63763684de37e1dee73ff714747bd
jvill171 <41810008+jvill171@users.noreply.github.com>
2023-07-23 21:38:42 -0500

    Fixed minor typo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ console.log(`I'd like one ${fruit}, please.`)
 console.log(`Here you go: ${fruit}`)
 console.log(`Delicious! May I have another?`)
 
-let remaining = remove(fruitChoices, fruit);
+let remaining = remove(fruits, fruit);
 
 console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`)
 
